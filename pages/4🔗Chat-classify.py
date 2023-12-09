bebac1ee6a56765edd2b83cc349018d4dfdda235
nabihawaseem <129088539+nabihawaseem@users.noreply.github.com>
2023-12-09 22:56:55 +0500

    Update 4🔗Chat-classify.py

--- pages/4🔗Chat-classify.py
+++ pages/4🔗Chat-classify.py
@@ -9,7 +9,6 @@ from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 from langchain.tools import Tool
 from langchain.utilities import GoogleSearchAPIWrapper
 from utils import get_user_data_from_database
-from classify import make_prediction
 from google.oauth2 import service_account
 from google.cloud import aiplatform
 import os
@@ -19,29 +18,28 @@ load_dotenv()  # load environment variables from .env
 
 # Page configuration
 st.set_page_config(page_title="LangChain with Vertex AI", page_icon="🌱")
-st.title("SPROUT - Farm 🌾🌱 ")
+st.title("SPROUT - Farm 🌾🌱")
 
 uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
-
-# Function to extract plant names from user data
-def extract_plant_names(user_data):
-    try:
-        plants_data = json.loads(user_data)
-        return [plant.get("name", "Unnamed Plant") for plant in plants_data if isinstance(plant, dict)]
-    except json.JSONDecodeError:
-        return []
 
 # Use the user_id from session state
 user_id = st.session_state.get('user_id')
 if user_id:
     user_data = get_user_data_from_database(user_id)
-    plant_names = extract_plant_names(user_data)
+    try:
+        plants_info = json.loads(user_data)
+        plant_names = [plant.get("name", "Unnamed Plant") for plant in plants_info if isinstance(plant, dict)]
+    except json.JSONDecodeError:
+        st.error("Error parsing user data")
+        plant_names = []
 else:
     st.error("No user ID found. Please sign up or log in.")
     plant_names = []
 
-# Display plant names as radio buttons
-selected_plant_name = st.radio("Select a plant:", plant_names)
+selected_plant = st.radio("Select a plant:", plant_names)
+
+# Find the data for the selected plant
+selected_plant_data = next((plant for plant in plants_info if plant.get("name") == selected_plant), None)
 
 # Access the credentials
 config = st.secrets["google_credentials"]
@@ -70,7 +68,7 @@ chat_model = ChatVertexAI(llm=llm)
 search = GoogleSearchAPIWrapper()
 GoogleSearch = Tool(
     name="Google Search",
-    description="Search Google for recent results and updated information on farming methods, and practices",
+    description="Search Google for recent results and updated information on farming methods and practices",
     func=search.run,
 )
 tools = [GoogleSearch]
@@ -79,22 +77,33 @@ chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=chat_model, tools=to
 executor = AgentExecutor.from_agent_and_tools(agent=chat_agent, tools=tools, memory=memory, return_intermediate_steps=True, handle_parsing_errors=True)
 
 # Chat
-if prompt := st.chat_input("Ask a question about farming"):
+if prompt := st.chat_input("Ask a question about your plant"):
     with st.chat_message("user"):
         st.write(prompt)
 
     with st.chat_message("assistant"):
         st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-
-        # Augment the prompt with selected plant information
-        augmented_prompt = f"{prompt} [Info about plant: {selected_plant_name}]"
+        
+        # Augment the prompt with specific plant data
+        if selected_plant_data:
+            plant_info = json.dumps(selected_plant_data)
+            augmented_prompt = f"{prompt} [Plant info: {plant_info}]"
+        else:
+            augmented_prompt = prompt
 
         if uploaded_file is not None:
             bytes_data = uploaded_file.getvalue()
             st.image(bytes_data, caption='Uploaded Image.', use_column_width=True)
-            predictions = make_prediction(bytes_data)
-            prediction_text = "Classifier result: " + ", ".join([str(prediction) for prediction in predictions])
-            augmented_prompt += " " + prediction_text
+            st.write("")
+
+            # Call the function with the image bytes and a label
+            embedding = encode_images_to_embeddings(image_bytes=bytes_data, label='Uploaded Image')
+            nearest = str(findneighbor_sample(embedding['image_embedding']))
+            print("nearest  ", nearest, type(nearest))
+            prompt = prompt + "this is info about user's plant(s): " + user_data + " this is the result of vector search on image uploaded, indicating the potential plant disease:" + nearest
+            print(prompt)
+        else:
+            prompt = prompt
 
         response = executor(augmented_prompt, callbacks=[st_cb])
         st.write(response["output"])
