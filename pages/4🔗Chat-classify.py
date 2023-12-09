8d5536ff52ac84984f36fff6ab30f10df2a8cac5
nabihawaseem <129088539+nabihawaseem@users.noreply.github.com>
2023-12-10 02:23:40 +0500

    Update 4🔗Chat-classify.py

--- pages/4🔗Chat-classify.py
+++ pages/4🔗Chat-classify.py
@@ -82,38 +82,37 @@ GoogleSearch = Tool(
     func=search.run,
 )
 tools = [GoogleSearch]
-
 chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=chat_model, tools=tools)
 executor = AgentExecutor.from_agent_and_tools(agent=chat_agent, tools=tools, memory=memory, return_intermediate_steps=True, handle_parsing_errors=True)
 
 prompt = st.chat_input(f"Ask a question about {selected_plant_name}:")
-    with st.chat_message("user"):
-        st.write(prompt)
+with st.chat_message("user"):
+    st.write(prompt)
 
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        
-        # Augment the prompt with specific plant data if a plant is selected
-        if selected_plant_data:
-            # Convert selected plant data to JSON string
-            plant_info = json.dumps(selected_plant_data)
-            augmented_prompt = f"{prompt} [Plant info: {plant_info}]"
-        else:
-            augmented_prompt = prompt
+with st.chat_message("assistant"):
+    st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+    
+    # Augment the prompt with specific plant data if a plant is selected
+    if selected_plant_data:
+        # Convert selected plant data to JSON string
+        plant_info = json.dumps(selected_plant_data)
+        augmented_prompt = f"{prompt} [Plant info: {plant_info}]"
+    else:
+        augmented_prompt = prompt
 
-        if uploaded_file is not None:
-            bytes_data = uploaded_file.getvalue()
-            st.image(bytes_data, caption='Uploaded Image.', use_column_width=True)
-            st.write("")
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        st.image(bytes_data, caption='Uploaded Image.', use_column_width=True)
+        st.write("")
 
-            # Call the function with the image bytes and a label
-            embedding = encode_images_to_embeddings(image_bytes=bytes_data, label='Uploaded Image')
-            nearest = str(findneighbor_sample(embedding['image_embedding']))
-            print("nearest  ", nearest, type(nearest))
-            prompt = prompt + "this is info about user's plant(s): " + user_data + " this is the result of vector search on image uploaded, indicating the potential plant disease:" + nearest
-            print(prompt)
-        else:
-            prompt = prompt
+        # Call the function with the image bytes and a label
+        embedding = encode_images_to_embeddings(image_bytes=bytes_data, label='Uploaded Image')
+        nearest = str(findneighbor_sample(embedding['image_embedding']))
+        print("nearest  ", nearest, type(nearest))
+        prompt = prompt + "this is info about user's plant(s): " + user_data + " this is the result of vector search on image uploaded, indicating the potential plant disease:" + nearest
+        print(prompt)
+    else:
+        prompt = prompt
 
-        response = executor(augmented_prompt, callbacks=[st_cb])
-        st.write(response["output"])
+    response = executor(augmented_prompt, callbacks=[st_cb])
+    st.write(response["output"])
