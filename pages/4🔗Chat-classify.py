1f1275e917e7122248f73056de8fe1dea10bb27f
Ayesha Imran <86928073+Ayesha-Imr@users.noreply.github.com>
2023-12-10 11:31:27 +0500

    Update 4🔗Chat-classify.py

--- pages/4🔗Chat-classify.py
+++ pages/4🔗Chat-classify.py
@@ -15,6 +15,12 @@ from vertexembed import encode_images_to_embeddings
 from vectorSearch import findneighbor_sample
 from utils import get_user_data_from_database
 
+# Imports main tools:
+from trulens_eval import TruChain, Feedback, Tru
+from trulens_eval import feedback, Feedback
+tru = Tru()
+tru.reset_database()
+
 
 import os
 import json
@@ -92,24 +98,48 @@ tools = [GoogleSearch]
 chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=chat_model, tools=tools)
 executor = AgentExecutor.from_agent_and_tools(agent=chat_agent, tools=tools, memory=memory, return_intermediate_steps=True, handle_parsing_errors=True)
 
-# Chat
-if prompt := st.chat_input("Ask a question about planting"):
-    with st.chat_message("user"):
-        st.write(prompt)
+hugs = feedback.Huggingface()
 
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        if uploaded_file is not None:
-            bytes_data = uploaded_file.getvalue()
-            st.image(bytes_data, caption='Uploaded Image.', use_column_width=True)
-            st.write("")
+f_lang_match = Feedback(hugs.language_match).on_input_output()
+pii = Feedback(hugs.pii_detection).on_output()
+pos = Feedback(hugs.positive_sentiment).on_output()
+tox = Feedback(hugs.toxic).on_output() 
 
-            # Call the classifier with the image bytes
-            predictions = make_prediction(bytes_data)
-            prediction_text = "This is the result of the classifier on the image uploaded, indicating the potential plant status: " + ", ".join([str(prediction) for prediction in predictions])
-            prompt = prompt + "this is info about user's plant(s): " + user_data + " Use the information about user's plant(s) to provide more relevant responses. If the user doesn't specify the plant, ask them to specify a plant first (if there are more than one)." +  " " + prediction_text
-            print(prompt)
-        else:
-            prompt = prompt + "this is info about user's plant(s): " + user_data + " Use the information about user's plant(s) to provide more relevant responses. If the user doesn't specify the plant, ask them to specify a plant first (if there are more than one)."
-        response = executor(prompt, callbacks=[st_cb])
-        st.write(response["output"])
+
+tru_recorder = TruChain(executor,
+    app_id='Chain1_ChatApplication',
+    feedbacks=[f_lang_match, pii, pos, tox])
+
+with tru_recorder as recording:
+
+    # Chat
+    if prompt := st.chat_input("Ask a question about planting"):
+        with st.chat_message("user"):
+            st.write(prompt)
+    
+        with st.chat_message("assistant"):
+            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            if uploaded_file is not None:
+                bytes_data = uploaded_file.getvalue()
+                st.image(bytes_data, caption='Uploaded Image.', use_column_width=True)
+                st.write("")
+    
+                # Call the classifier with the image bytes
+                predictions = make_prediction(bytes_data)
+                prediction_text = "This is the result of the classifier on the image uploaded, indicating the potential plant status: " + ", ".join([str(prediction) for prediction in predictions])
+                prompt = prompt + "this is info about user's plant(s): " + user_data + " Use the information about user's plant(s) to provide more relevant responses. If the user doesn't specify the plant, ask them to specify a plant first (if there are more than one)." +  " " + prediction_text
+                print(prompt)
+            else:
+                prompt = prompt + "this is info about user's plant(s): " + user_data + " Use the information about user's plant(s) to provide more relevant responses. If the user doesn't specify the plant, ask them to specify a plant first (if there are more than one)."
+            response = executor(prompt, callbacks=[st_cb])
+            st.write(response["output"])
+
+# Displaying Results
+with st.expander("Detailed Evaluation Results"):
+    records, feedback = tru.get_records_and_feedback(app_ids=[])
+    st.dataframe(records)
+    
+with st.container():
+    st.header("Evaluation")    
+    st.dataframe(tru.get_leaderboard(app_ids=[]))
+    st.dataframe(feedback)
