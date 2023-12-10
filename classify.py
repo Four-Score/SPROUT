e00b11ceb4b95be04325ff2d06368c7908a65c5e
nabihawaseem <129088539+nabihawaseem@users.noreply.github.com>
2023-12-10 23:09:50 +0500

    Update classify.py

--- classify.py
+++ classify.py
@@ -1,48 +1,33 @@
 from dotenv import load_dotenv
 import os
 import streamlit as st
-from google.cloud import aiplatform
-from google.oauth2 import service_account
-import base64
-from google.cloud.aiplatform.gapic.schema import predict as schema_predict
+from tensorflow.keras.models import load_model
 from PIL import Image
+import numpy as np
 import io
-import json
 
 # Load environment variables from .env file
 load_dotenv()
-import toml
-#config = st.secrets["google_credentials"]
-# Construct a credentials object from the dictionary
 
-#creds = service_account.Credentials.from_service_account_info(config)
-# Parse the service account credentials from the environment variable
-
+# Load your trained model
+model_path = 'path/to/your/model/my_model.hdf5'  # Update this path
+model = load_model(model_path)
 
 # Define the function to make a prediction
 def make_prediction(image_bytes):
-    client_options = {"api_endpoint": f"{os.getenv('LOCATION')}-aiplatform.googleapis.com"}
-    client = aiplatform.gapic.PredictionServiceClient(client_options=client_options, credentials=creds)
-    
-    encoded_content = base64.b64encode(image_bytes).decode("utf-8")
-    instance = schema_predict.instance.ImageClassificationPredictionInstance(
-        content=encoded_content,
-    ).to_value()
-    instances = [instance]
-    parameters = schema_predict.params.ImageClassificationPredictionParams(
-        confidence_threshold=0.5,
-        max_predictions=5,
-    ).to_value()
-    endpoint = client.endpoint_path(
-        project=os.getenv("PROJECT_ID_CODE"),
-        location=os.getenv("LOCATION"),
-        endpoint=os.getenv("ENDPOINT_ID")
-    )
-    response = client.predict(endpoint=endpoint, instances=instances, parameters=parameters)
+    # Load the image from bytes and preprocess it
+    img = Image.open(io.BytesIO(image_bytes))
+    img = img.resize((256, 256))  # Update the size to what your model expects
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array.astype('float32') / 255.0  # Normalize if your model expects
 
-    # Parse the response to make it human-readable
-    predictions = []
-    for prediction in response.predictions:
-        predictions.append(dict(prediction))
+    # Make a prediction
+    predictions = model.predict(img_array)
 
-    return predictions
+    # Convert predictions to a human-readable format
+    formatted_predictions = np.argmax(predictions, axis=1).tolist()
+
+    return formatted_predictions
+
+# Streamlit UI code goes here...
