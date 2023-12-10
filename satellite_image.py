78a3166dec44c70a95bdf46940947d26741279c5
Maha Khan <129088724+mahakhan-mk@users.noreply.github.com>
2023-12-10 11:07:51 +0500

    Update satellite_image.py

--- satellite_image.py
+++ satellite_image.py
@@ -28,58 +28,50 @@ def get_most_recent_images(polygon_id, api_key):
 
 def display_images_and_stats(image_data, api_key):
     if image_data:
-        col1, col2 = st.columns([1, 3])  # Adjust the ratio as per your UI needs
+        # Display the NDVI image with a heading
+        st.subheader("This is your NDVI Image")
+        ndvi_image_url = image_data['image']['ndvi']
+        st.image(ndvi_image_url, caption="Most Recent NDVI Image", width=150)
 
-        with col1:
-            # Display the NDVI image
-            ndvi_image_url = image_data['image']['ndvi']
-            st.image(ndvi_image_url, caption="Most Recent NDVI Image", width=150)
-            
-            # Add a button to download the NDVI image
-            if ndvi_image_url:
-                # Fetch the image from the URL
-                img_response = requests.get(ndvi_image_url)
-                if img_response.status_code == 200:
-                    # Use the download button to download the image
-                    st.download_button(
-                        label="Download NDVI Image",
-                        data=img_response.content,
-                        file_name="ndvi_image.jpg",
-                        mime="image/jpg"
-                    )
-                else:
-                    st.error("Failed to download NDVI image.")
-        with col2:
-            # Create an empty DataFrame to store the statistics
-            stats_df = pd.DataFrame()
+        # Add a button to download the NDVI image
+        if ndvi_image_url:
+            img_response = requests.get(ndvi_image_url)
+            if img_response.status_code == 200:
+                st.download_button(
+                    label="Download NDVI Image",
+                    data=img_response.content,
+                    file_name="ndvi_image.jpg",
+                    mime="image/jpg"
+                )
+            else:
+                st.error("Failed to download NDVI image.")
 
-            # Iterate over each index type to fetch and display its statistics
-            for index_type in ['ndvi', 'evi', 'evi2', 'nri', 'dswi', 'ndwi']:
-                # Correctly construct the statistics URL
-                base_url, _ = image_data['stats'][index_type].split('?')
-                stat_url = f"{base_url}?appid={api_key}"
-                
-                # Print the URL for debugging purposes
-                print(f"Constructed URL for {index_type} statistics: {stat_url}")
-                
-                # Make the request to the API
-                response = requests.get(stat_url)
-                if response.status_code == 200:
-                    stats = response.json()
-                    # Append the stats to the DataFrame, rounding to two decimal places
-                    stats_df[index_type.upper()] = [
-                        round(stats.get('max', 'N/A'), 2),
-                        round(stats.get('mean', 'N/A'), 2),
-                        round(stats.get('median', 'N/A'), 2),
-                        round(stats.get('min', 'N/A'), 2),
-                        round(stats.get('std', 'N/A'), 2),
-                        round(stats.get('num', 'N/A'), 2)
-                    ]
-                else:
-                    st.error(f"Failed to load {index_type.upper()} data: {response.text}")
+        # Display the statistics with a heading
+        st.subheader("This is your Statistics")
+        # Initialize an empty list to store the statistics
+        stats_data = []
 
-            # Set the index of the DataFrame for better readability
-            stats_df.index = ['Max', 'Mean', 'Median', 'Min', 'Standard Deviation', 'Number of Pixels']
+        # Iterate over each index type to fetch and display its statistics
+        for index_type in ['ndvi', 'evi', 'evi2', 'nri', 'dswi', 'ndwi']:
+            base_url, _ = image_data['stats'][index_type].split('?')
+            stat_url = f"{base_url}?appid={api_key}"
+            response = requests.get(stat_url)
+            if response.status_code == 200:
+                stats = response.json()
+                # Append the stats to the list, rounded to 2 decimal places
+                stats_data.append([
+                    index_type.upper(),
+                    round(float(stats.get('max', 'N/A')), 2) if 'max' in stats else 'N/A',
+                    round(float(stats.get('mean', 'N/A')), 2) if 'mean' in stats else 'N/A',
+                    round(float(stats.get('median', 'N/A')), 2) if 'median' in stats else 'N/A',
+                    round(float(stats.get('min', 'N/A')), 2) if 'min' in stats else 'N/A',
+                    round(float(stats.get('std', 'N/A')), 2) if 'std' in stats else 'N/A',
+                    round(float(stats.get('num', 'N/A')), 2) if 'num' in stats else 'N/A'
+                ])
+            else:
+                st.error(f"Failed to load {index_type.upper()} data: {response.text}")
 
-            # Display the DataFrame as a table
-            st.table(stats_df.T)  # Transpose for better layout
+        # Convert the list of statistics to a DataFrame
+        stats_df = pd.DataFrame(stats_data, columns=['Index', 'Max', 'Mean', 'Median', 'Min', 'Standard Deviation', 'Number of Pixels'])
+        # Display the DataFrame as a table
+        st.table(stats_df.set_index('Index'))  # Set the 'Index' column as the index of the DataFrame
